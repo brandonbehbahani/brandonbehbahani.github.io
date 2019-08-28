f2133c6efe3fd1cac096c001bf578136d31fad4f
komronhussaini <brandonbehbahani@gmail.com>
2019-08-27 21:19:24 -0400

    making things look better

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 function displayMobileMenu(){
   console.log("mobile menu");
   var x = document.getElementById("navigation");
+  var y = document.getElementById("menu-tab");
     if (x.className === "navigation") {
         x.className = "mobileMenu";
+        y.src = "./images/x.png";
     } else {
         x.className = "navigation";
+        y.src = "./images/menu.png";
     }
 }
 
